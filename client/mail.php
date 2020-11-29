4944f583cb3ba9b91a9ae36583d85ee5d33d63b1
GitAlmaz <almazinhs@gmail.com>
2020-11-30 04:19:13 +0600

    add phpmailer

--- client/mail.php
+++ client/mail.php
@@ -1,17 +1,20 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-use PHPMailer\PHPMailer;
-use PHPMailer\Exception;
-$mail = trim($_POST['email']);
-$name = trim($_POST['name']);
-$birthday = trim($_POST['birthday']);
-$birth_location = trim($_POST['birth_location']);
-$citizenship = trim($_POST['citizenship']);
-$place_of_live = trim($_POST['place_of_live']);
-$contact_number = trim($_POST['contact_number']);
-$height_weight = trim($_POST['height_weight']);
-$photos_base = trim($_POST['photos']);
-$photos = base64_encode($photos_base);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+$data = json_decode($_POST[0]);
+$mail = $data['email'];
+$name = $data['name'];
+$birthday = $data['birthday'];
+$birth_location = $data['birth_location'];
+$citizenship = $data['citizenship'];
+$place_of_live = $data['place_of_live'];
+$contact_number = $data['contact_number'];
+$height_weight = $data['height_weight'];
+$photos = json_decode($data['photos']);
 $message = "\n
 Имя: " . $name . "\n
 E-mail:" . $email . "\n
@@ -21,26 +24,16 @@ E-mail:" . $email . "\n
 Место жительство:" . $place_of_live . "\n
 Контактный номер:" . $contact_number . "\n
 Рост/вес:" . $height_weight . "\n
-Фотографии:" . $height_weight . "\n";
-$email = new PHPMailer();
-$email->SetFrom('from Artem');
+Фотографии:" . $photos . "";
+$email = new PHPMailer(true);
+$email->SetFrom('amoresncr@gmail.com');
 $email->Subject = 'Новая заявка!';
-$email->Body      = $bodytext;
+$email->Body      = $message;
 $email->AddAddress( 'belozerov.inhs.artem@gmail.com' );
-$email->AddAttachment( $photos , 'Фотографии' );
+foreach ($photos as $photo) {
+    $res = base64_decode(str_replace(" ", "+", substr($base, strpos($base, ","))));
+    $email->AddStringAttachment($res,'filename.png',"base64", "image/png");
+}
 return $email->Send();
-// $to = "Amoresincero1@yandex.ru";
-
-// $message = $photos;
-
-// $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
-// $subject = "Новая заявка";
-
-// $mail = mail($to, $subject, $message, $headers);
-
-// if ($mail) {
-// 	echo 1;
-// } else {
-// 	echo 0;
-// }
+echo $message;
 
